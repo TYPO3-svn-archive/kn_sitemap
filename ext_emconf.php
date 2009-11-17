facd408ad747dea810265ce2fe2704234f4a3447
martin.kuster <martin.kuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-17 14:28:33 +0000

    Moved to kn_lib->jquery
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kn_sitemap/trunk@26631 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'typo3' => '4.1-0.0.0',
 			'php' => '5.1.0-0.0.0',
-			'jquery' => '1.2.0-',
+			'kn_lib' => '5.1.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
